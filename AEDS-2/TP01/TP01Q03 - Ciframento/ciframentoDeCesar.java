d33e040d825b83eb273c29e0ae68cd4db33cc546
David Nunes <ddnr03@gmail.com>
2025-02-17 15:52:20 -0300

    TP01Q03 - Ciframento

---
+++ AEDS-2/TP01/TP01Q03 - Ciframento/ciframentoDeCesar.java
@@ -0,0 +1,40 @@
+/*
+   ==UserScript==
+ @name         TP01Q03 - Ciframento
+ @namespace    https://github.com/Ddavidi/PUC-MINAS
+ @description  VERDE PUC MINAS - TP01Q03 - Ciframento
+ @author       @ddavidi_
+   ==/UserScript==
+*/
+
+import java.util.Scanner;
+
+class ciframentoDeCesar {
+    public static void cifrandoEntrada(String entrada){
+        int tam = entrada.length();
+
+        for(int i=0; i<tam; i++){
+            int caracterCifrado = ((int) entrada.charAt(i)) + 3;
+            System.out.print((char) caracterCifrado);
+        }
+
+        System.out.println();
+    }
+
+    public static boolean isFim(String entrada){
+        return entrada.charAt(0) == 'F' && entrada.charAt(1) == 'I' && entrada.charAt(2) == 'M';
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        String entrada = scan.nextLine();
+
+        while(!isFim(entrada)){
+            cifrandoEntrada(entrada);
+            entrada = scan.nextLine();
+        }
+
+        scan.close();
+    }
+}
