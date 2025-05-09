00882854f6f82a9a6150ae7921dfe5fa78f70641
David Nunes <ddnr03@gmail.com>
2025-05-08 22:01:56 -0300

    Aquecimento Java - incompleto

---
+++ AEDS-2/ESTUDO/Prova-2/AquecimentoJava.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class AquecimentoJava {
+
+    public static class Pessoa {
+        String nome;
+        int peso;
+
+        public Pessoa(String nome, int peso){
+            this.nome = nome;
+            this.peso = peso;
+        }
+
+        public static Pessoa ler(String linha){
+            String[] linhaDividida = linha.split(" ");
+            String nome = "";
+            for(int i=0; i<linhaDividida.length-1;i++){
+                nome += linhaDividida[i] + " ";
+            }
+
+            nome = nome.trim();
+
+            int peso = Integer.parseInt(linhaDividida[linhaDividida.length-1]);
+
+            return new Pessoa(nome, peso);
+        }
+
+        public static void ordenar(Pessoa[] atletas){
+            
+        }
+
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        scan.nextLine(); // quebra de linha pendente
+        Pessoa[] atletas = new Pessoa[N];
+
+        // cria preenchendo os atletas
+        for(int i=0; i<N; i++){
+            String linha = scan.nextLine();
+            atletas[i] = Pessoa.ler(linha);
+        }
+
+        for(int i=0; i<N; i++){
+            System.out.println(atletas[i].nome + " " + atletas[i].peso);
+        }
+
+        scan.close();
+    }
+}
