2198fc67123a33eba42abf4ebdb12cc7b196b9e7
David Nunes <ddnr03@gmail.com>
2026-06-22 22:00:51 -0300

    Corrige fios partidos e sobreposicao de texto de vez

--- CIRCUITOS ELETRICOS/code/draw_thevenin_vth_arrows.py
+++ CIRCUITOS ELETRICOS/code/draw_thevenin_vth_arrows.py
@@ -7,39 +7,46 @@ img_dir = os.path.join(base_dir, "_base_dados_ia", "imagens_geradas")
 os.makedirs(img_dir, exist_ok=True)
 
 with schemdraw.Drawing(file=os.path.join(img_dir, "thevenin_vth_arrows.png"), show=False) as d:
-    d.config(unit=4)
+    # 32V Source
+    source = elm.SourceV().up().label('32V')
+    d += source
     
-    d += elm.SourceV().up().label('32V')
+    # Left branch
+    d += elm.Line().right().length(1.5).label('← I', color='red')
+    d += elm.Resistor().right().length(3).label('4Ω')
+    d += elm.Line().right().length(1.5)
     
-    d += elm.Line().right().length(1)
-    d += elm.Resistor().right().label('4Ω', loc='bot').label('← I', loc='top', color='red')
-    d += elm.Line().right().length(1)
-    d += elm.Dot().label('Nó C', loc='top', ofst=0.5)
+    # Node C
+    d += elm.Dot().label('Nó C', loc='top')
     
+    # Down branch 1 (12 ohm)
     d.push()
-    d += elm.Line().down().length(0.8)
-    d += elm.Resistor().down().label('12Ω', loc='bot').label('I ↓', loc='top', color='red')
-    d += elm.Line().down().length(0.8)
-    bot_mid1 = d.here
+    d += elm.Line().down().length(1.5).label('I ↓', loc='bot', color='red')
+    d += elm.Resistor().down().length(3).label('12Ω')
+    bot_mid = d.here
+    d += elm.Line().left().tox(source.start)
     d += elm.Ground()
     d.pop()
     
-    d += elm.Line().right().length(2)
+    # Wire to next branch
+    d += elm.Line().right().length(2.5)
     
+    # Down branch 2 (2A source)
     d.push()
-    d += elm.Line().down().length(0.8)
-    d += elm.SourceI().down().reverse().label('2A', loc='bot').label('↑ 2A (Entrando)', loc='top', color='blue')
-    bot_mid2 = d.here
+    d += elm.Line().down().length(1.5).label('↑ 2A\n(Entrando)', loc='bot', color='blue')
+    d += elm.SourceI().down().reverse().length(3).label('2A')
+    d += elm.Line().left().tox(bot_mid)
     d.pop()
     
-    d += elm.Line().right().length(1)
-    d += elm.Resistor().right().label('1Ω', loc='top').label('(i=0A)', loc='bot', color='red')
-    d += elm.Line().right().length(1)
-    d += elm.Dot().label('Terminal A', loc='right')
+    # Right branch
+    d += elm.Line().right().length(1.5)
+    d += elm.Resistor().right().length(3).label('1Ω\n(i=0A)')
+    d += elm.Line().right().length(1.5)
     
-    d += elm.Line().down().toy(bot_mid2).color('white')
-    d += elm.Dot().label('Terminal B', loc='right')
+    # Terminals
+    d += elm.Dot().label('Terminal A (+Vth)', loc='right')
+    d += elm.Line().down().toy(bot_mid).color('white')
+    d += elm.Dot().label('Terminal B (-Vth)', loc='right')
     
-    d += elm.Line().left().to(bot_mid2)
-    d += elm.Line().left().to(bot_mid1)
-    d += elm.Line().left().to((0,0))
+    # Bottom wire
+    d += elm.Line().left().tox(bot_mid)
