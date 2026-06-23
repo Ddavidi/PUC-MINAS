5984df6d694e4f4b3c503b1b8bdca424809aba51
David Nunes <ddnr03@gmail.com>
2026-06-22 21:50:49 -0300

    Corrige sobreposicao de texto na imagem detalhada de Thevenin

--- CIRCUITOS ELETRICOS/code/draw_thevenin_vth_arrows.py
+++ CIRCUITOS ELETRICOS/code/draw_thevenin_vth_arrows.py
@@ -7,14 +7,15 @@ img_dir = os.path.join(base_dir, "_base_dados_ia", "imagens_geradas")
 os.makedirs(img_dir, exist_ok=True)
 
 with schemdraw.Drawing(file=os.path.join(img_dir, "thevenin_vth_arrows.png"), show=False) as d:
-    d += elm.SourceV().up().label('32V')
-    # Resistor com seta de corrente saindo do Nó C para a esquerda
-    d += elm.Resistor().right().label('4Ω').label('← I', loc='top', color='red')
+    d += elm.SourceV().up().label('32V', loc='left')
+    
+    # Resistor 4 ohms: Valor embaixo, Seta de corrente vermelha em cima
+    d += elm.Resistor().right().label('4Ω', loc='bot').label('← I fugindo', loc='top', color='red')
     d += elm.Dot().label('Nó C', loc='top')
     
     d.push()
-    # Resistor descendo com seta de corrente descendo
-    d += elm.Resistor().down().label('12Ω').label('I ↓', loc='right', color='red')
+    # Resistor 12 ohms: Valor na esquerda, Seta vermelha na direita
+    d += elm.Resistor().down().label('12Ω', loc='left').label('I fugindo ↓', loc='right', color='red')
     d += elm.Line().left()
     d += elm.Ground()
     d.pop()
@@ -22,14 +23,14 @@ with schemdraw.Drawing(file=os.path.join(img_dir, "thevenin_vth_arrows.png"), sh
     d += elm.Line().right().length(2)
     
     d.push()
-    # Fonte de corrente entrando
-    d += elm.SourceI().down().reverse().label('2A').label('↑ 2A\n(Entrando)', loc='right', color='blue')
+    # Fonte 2A: Valor na esquerda, explicacao azul na direita
+    d += elm.SourceI().down().reverse().label('2A', loc='left').label('↑ 2A\n(Entrando)', loc='right', color='blue')
     d += elm.Line().left().length(2)
     d.pop()
     
-    # Ramo A com zero corrente
-    d += elm.Resistor().right().label('1Ω\n(i=0A)', color='red')
-    d += elm.Dot().label('A (+Vth)', loc='right')
+    # Ramo A com zero corrente: Valor em cima, texto vermelho embaixo
+    d += elm.Resistor().right().label('1Ω', loc='top').label('(i=0A)', loc='bot', color='red')
+    d += elm.Dot().label('Terminal A (+Vth)', loc='right')
     d += elm.Line().down().length(3).color('white')
-    d += elm.Dot().label('B (-Vth)', loc='right')
+    d += elm.Dot().label('Terminal B (-Vth)', loc='right')
     d += elm.Line().left().length(3 + 2 + 3)
