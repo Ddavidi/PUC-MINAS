e1f590fa07a7639550f115e4497fcf9bea580393
David Nunes <ddnr03@gmail.com>
2026-06-24 17:19:09 -0300

    Expande significativamente a escala da imagem do problema 7.17 para nao encavalar legendas

--- CIRCUITOS ELETRICOS/code/draw_problema_7_17.py
+++ CIRCUITOS ELETRICOS/code/draw_problema_7_17.py
@@ -6,34 +6,36 @@ img_dir = r"c:\Users\DD\Documents\DD\PUC MINAS\PUC-MINAS\CIRCUITOS ELETRICOS\_ba
 os.makedirs(img_dir, exist_ok=True)
 
 with schemdraw.Drawing(file=os.path.join(img_dir, "problema_7_17.png"), show=False) as d:
-    d.config(unit=3.0, fontsize=16)
+    d.config(unit=5.0, fontsize=16)
     
     # Fio inferior
-    d += elm.Line().right().at((0,0)).length(6)
+    d += elm.Line().right().at((0,0)).length(8)
     
     # Ramo esquerdo (Fonte)
-    d += elm.SourceV().up().at((0,0)).label('v(t)', loc='left')
+    d += elm.Line().up().at((0,0)).length(1.5)
+    d += elm.SourceV().up().label('v(t)', loc='left')
+    d += elm.Line().up().length(1.5)
     top_left = d.here
     
     # Ramo superior (Resistor 1 ohm)
-    d += elm.Resistor().right().at(top_left).label('1 Ω', loc='top').length(3)
+    d += elm.Resistor().right().at(top_left).label('1 Ω', loc='top').length(4)
     top_mid = d.here
     
     # Ramo do meio (Resistor 3 ohm + Indutor 1/4 H)
-    d += elm.Resistor().down().at(top_mid).label('3 Ω', loc='left').length(1.5)
+    d += elm.Resistor().down().at(top_mid).label('3 Ω', loc='left').length(3)
     mid_node = d.here
-    d += elm.Inductor2().down().at(mid_node).label('1/4 H', loc='left').toy(0)
+    d += elm.Inductor2().down().at(mid_node).label('1/4 H', loc='left').length(3)
     bot_mid = (top_mid[0], 0)
     
     # Seta de corrente
-    d += elm.Line(arrow='->').down().at((top_mid[0]+0.8, top_mid[1]-0.2)).length(1).color('red').label('$i(t)$', loc='right')
+    d += elm.Line(arrow='->').down().at((top_mid[0]+1.0, top_mid[1]-0.5)).length(2).color('red').label('$i(t)$', loc='right')
     
     # Terminais abertos na direita
-    d += elm.Line().right().at(top_mid).length(3)
+    d += elm.Line().right().at(top_mid).length(4)
     top_right = d.here
     d += elm.Dot(open=True)
     
-    d += elm.Line().right().at(bot_mid).length(3)
+    d += elm.Line().right().at(bot_mid).length(4)
     bot_right = d.here
     d += elm.Dot(open=True)
     
--- CIRCUITOS ELETRICOS/code/draw_problema_7_17_thevenin.py
+++ CIRCUITOS ELETRICOS/code/draw_problema_7_17_thevenin.py
@@ -6,27 +6,27 @@ img_dir = r"c:\Users\DD\Documents\DD\PUC MINAS\PUC-MINAS\CIRCUITOS ELETRICOS\_ba
 os.makedirs(img_dir, exist_ok=True)
 
 with schemdraw.Drawing(file=os.path.join(img_dir, "problema_7_17_thevenin.png"), show=False) as d:
-    d.config(unit=3.0, fontsize=16)
+    d.config(unit=5.0, fontsize=16)
     
     # Fio inferior
-    d += elm.Line().right().at((0,0)).length(3).color('blue')
+    d += elm.Line().right().at((0,0)).length(4).color('blue')
     bot_mid = d.here
     
     # Ramo esquerdo (Curto)
-    d += elm.Line().up().at((0,0)).color('blue').label('Curto\n$v(t)=0$', loc='left')
+    d += elm.Line().up().at((0,0)).length(6).color('blue').label('Curto\n$v(t)=0$', loc='left')
     top_left = d.here
     
     # Ramo superior
-    d += elm.Resistor().right().at(top_left).label('1 Ω', loc='top').length(3).color('blue')
+    d += elm.Resistor().right().at(top_left).label('1 Ω', loc='top').length(4).color('blue')
     top_mid = d.here
     
     # Ramo do meio
-    d += elm.Resistor().down().at(top_mid).label('3 Ω', loc='left').length(1.5).color('blue')
+    d += elm.Resistor().down().at(top_mid).label('3 Ω', loc='left').length(3).color('blue')
     mid_node = d.here
     
     # Terminais de Thevenin
     d += elm.Dot(open=True).at(mid_node).label('A', loc='top', color='green')
     d += elm.Dot(open=True).at(bot_mid).label('B', loc='bottom', color='green')
-    d += elm.Label().at((mid_node[0] + 1.2, (mid_node[1] + bot_mid[1])/2)).label('Visão para\n$R_{eq}$', color='green')
+    d += elm.Label().at((mid_node[0] + 1.5, (mid_node[1] + bot_mid[1])/2)).label('Visão para\n$R_{eq}$', color='green')
 
 print("Gerado problema_7_17_thevenin.png")
