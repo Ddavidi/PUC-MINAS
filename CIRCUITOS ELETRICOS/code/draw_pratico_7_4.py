e3ee77cbd82211ecd1a604da223d1512ea096ad5
David Nunes <ddnr03@gmail.com>
2026-06-23 21:23:13 -0300

    Corrige legendas sobrepostas no diagrama didatico

--- CIRCUITOS ELETRICOS/code/draw_pratico_7_4.py
+++ CIRCUITOS ELETRICOS/code/draw_pratico_7_4.py
@@ -109,13 +109,13 @@ with schemdraw.Drawing(file=os.path.join(img_dir, "cap7_pratico_7_4_req.png"), s
 with schemdraw.Drawing(file=os.path.join(img_dir, "cap7_pratico_7_4_curto.png"), show=False) as d:
     d.config(unit=3.5, fontsize=14)
     
-    d += elm.Line().down().length(1).label('Corrente Total\nchegando do 12Ω', loc='bot')
+    d += elm.Line().down().length(2).label('Corrente Total\nDescendo', loc='left')
     node_B = d.here
     
-    d += elm.Line().down().length(3).color('blue').label('Fio Liso (Indutor)\n0Ω de Resistência\n100% da Corrente', loc='left', color='blue')
+    d += elm.Line().down().length(4).color('blue').label('Fio Liso (0Ω)\n100% da Água', loc='left', color='blue')
     bot_mid = d.here
     
-    d += elm.Resistor().right().at(node_B).length(4).color('gray').label('Resistor 5Ω\nCaminho com obstáculo\n0% da Corrente', loc='top', color='gray')
+    d += elm.Resistor().right().at(node_B).length(5).color('gray').label('Resistor de 5Ω\nCaminho Difícil\n0% da Água', loc='bot', color='gray')
     node_C = d.here
     
     d += elm.Line().down().length(3).color('gray')
